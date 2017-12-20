4a59e8f47740df234d0b2b407beaeb5f1e5bb75e
TheCodingLand <aureussz@gmail.com>
2017-12-20 21:49:59 +0000

    fixed small issues and comments

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ t = Ticket()
 t.Title="ticket"
 t.Description="ticket"
 
-url='http://localhost' #URL ot the Server
-headers = { 'Content-Type': 'text/json', 'charset': 'utf-8' }
+url = 'https://api.github.com/some/endpoint' # custom url
+payload = {'some': 'data'}
+headers = {'content-type': 'application/json'}
 
 
 def createTicket(ticket):
-    return requests.post(url,headers, json.dumps(t.serialize()))
+    r = requests.post(url, data=json.dumps(t.serialize()), headers=headers)
+    
+    
 
 
-print(json.dumps(t.serialize()))
+print(json.dumps(t.serialize())) # Just to see the data
   
-#print(createTicket(t))
+createTicket(t))
